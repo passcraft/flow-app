ce95eac675a060baea1c1ed5a1a6c861dfdc57e2
sarim2000 <sarimbleedblue@gmail.com>
2023-02-18 18:10:30 +0530

    s

--- pages/index.jsx
+++ pages/index.jsx
@@ -150,6 +150,7 @@ const Home = () => {
                   <div class="mb-6">
                     <input
                       type="text"
+                      name="email"
                       class="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
                       id="exampleFormControlInput2"
                       placeholder="Email address"
